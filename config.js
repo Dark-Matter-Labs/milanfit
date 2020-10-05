69ab8d76aa9b135143a9b6c07efe3464cf5b974f
Gurden Batra <gurden.batra@gmail.com>
2020-10-05 23:56:03 +0300

    changing max zoom

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var dataUrl = 'data/milan.csv';
-var maxZoom = 20;
+var maxZoom = 19;
 var fieldSeparator = ';';
 var baseUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
