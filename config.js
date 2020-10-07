91e36d8ac220678fc4df761c3881b9ca21b6fd57
Gurden Batra <gurden.batra@gmail.com>
2020-10-07 13:13:26 +0300

    reverting back to ; delimeter

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var dataUrl = 'data/milan.csv';
 var maxZoom = 19;
-var fieldSeparator = ',';
+var fieldSeparator = ';';
 var baseUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = 'abc';
